3c566c9d78db353f6ffef85f8b86611976aca988
dev-Lesser <dev.jongmoon@gmail.com>
2022-02-06 13:00:00 +0900

    :art: Edit CORS configure & test

--- api-wrapper/app.py
+++ api-wrapper/app.py
@@ -1,21 +1,29 @@
-from chalice import Chalice, Response
+from chalice import Chalice, Response, CORSConfig
 from urllib import request
 app = Chalice(app_name='api-wrapper')
 
-EC2_IP = 'xxx.xxx.xxx.xxx'
+EC2_IP = '127.0.0.1:8000' # local
 HEADERS = {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*'
 }
 
-@app.route('/index', methods=['GET'], cors=False)
+cors_config = CORSConfig(
+    allow_origin='*',
+    allow_headers=['*'],
+    max_age=3600,
+    expose_headers=['*'],
+    allow_credentials=True
+)
+
+@app.route('/index', methods=['GET'], cors=cors_config)
 def roi_attrs():    
     e = app.current_request.to_dict()
     params = e.get('query_params')
-    param = params.get('param')
-    url = 'http://%s/api/index?param=%s' %(EC2_IP, param)
+    data = params.get('data')
+    url = 'http://%s/index?data=%s' %(EC2_IP, data)
+    print(url)
     with request.urlopen(url) as response:
         res = response.read()
     return Response(body=res,
-            headers=HEADERS,
             status_code=200)
--- server/main.py
+++ server/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 import uvicorn
 
 app = FastAPI()
@@ -15,11 +16,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/api/index")
-async def index():
+@app.get("/index")
+async def index(data: str,):
     return JSONResponse(
             status_code=200,
-            content='test'
+            content=data
         )
 
 
